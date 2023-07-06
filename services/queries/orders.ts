f66a7a702adf281f3db1c02bbcad9461f160a5e8
JP <alias99002@outlook.com>
2023-07-06 16:44:22 -0500

    creating order by user(#28)
    bug with stripe payment

---
+++ services/queries/orders.ts
@@ -0,0 +1,55 @@
+import Cookie  from 'js-cookie';
+import { endPoints } from "./endPoints";
+
+
+
+
+export async function createOrder(orderData: { paid: boolean, status: string}) {
+  const { paid, status }= orderData
+  
+    const token = Cookie.get('token');
+    
+  //Set the Authorization header with the token
+    const headers = {
+      'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      };
+
+  const response = await fetch(`${endPoints.orders.createOrder}`, 
+  {
+    method: 'POST',
+    headers: headers,
+  
+    //make sure to serialize your JSON body
+    body: JSON.stringify({
+        paid, 
+        status
+    })
+  } 
+  );
+  const data = await response.json();
+  // console.log(data, 'is updated');
+ return data;
+}
+
+
+export async function getOrdersByCustomer(id: string): Promise<ResumeOrder[]> {
+
+    const token = Cookie.get('token');
+    //Set the Authorization header with the token
+  
+    const headers = {
+        Authorization: `Bearer ${token}`
+      };
+      
+  const response = await fetch(`${endPoints.orders.myOrders(id)}`, {
+    method: 'GET',
+    headers: headers
+  });
+  const data = await response.json();
+  // console.log(data, ' all orders')
+  return data;
+  
+  }
+  
