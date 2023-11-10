1df1584a9920bbe9c36d3dbfebf654e1fd662d2a
JP <alias99002@outlook.com>
2023-11-10 14:37:07 -0500

    testing new graphql database

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const runtime = 'edge';
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = req.body;
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        body
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch products');
+    }
+
+    const data = await response.json();
+    return NextResponse.json({ data })
+  
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    NextResponse.json({ message: 'Internal Server Error' });
+  }
+}
